8891ebb5bcc1c2ee5f99eacc7fbffe84c5e11804
gabriel <74655512+gabyformula1@users.noreply.github.com>
2023-05-27 12:27:30 +0200

    Update ET3.ino
    ET3 Correct

--- ET3.ino
+++ ET3.ino
@@ -70,26 +70,57 @@ static const int16_t filter_taps[FILTER_TAP_NUM] = {
   21546
 };
 
+// ADC features
+const int bufferSize = 7500;
+uint16_t adcBuffer[bufferSize];
+volatile bool bufferFull = false;
+
+// ADC configuration
 void setupADC() {
-  PMC->PMC_PCER1 |= PMC_PCER1_PID37;     // Enable the ADC peripheral
-  ADC->ADC_MR = ADC_MR_PRESCAL(255)      // Set the prescaler to 255
-                | ADC_MR_STARTUP_SUT64   // Set the startup time to 64 periods of ADC_CLK
-                | ADC_MR_TRACKTIM(15)    // Set the tracking time to 15 periods of ADC_CLK
-                | ADC_MR_SETTLING_AST3;  // Set the settling time to 17 periods of ADC_CLK
-  ADC->ADC_CHER = ADC_CHER_CH7;          // Enable channel 7 (A0)
-  // Configure Timer Counter 0 Channel 0 (TC0) for samplingFrequency
-  PMC->PMC_PCER0 |= PMC_PCER0_PID27;  // Enable the TC0 peripheral
-  TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK4 | TC_CMR_CPCTRG;
-  // Set the clock source to TCLK4 (MCK / 128, 84 MHz / 128 = 656.25 kHz)
-  // Enable the RC compare trigger
-  // Set the RC value for a samplingFrequency Hz frequency
-  TC0->TC_CHANNEL[0].TC_RC = 656250 / samplingFrequency - 1;
-  // Enable the RC compare interrupt
-  TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
-  // Enable the TC0_IRQn interrupt in the NVIC
-  NVIC_EnableIRQ(TC0_IRQn);
+  PMC->PMC_PCER1 |= PMC_PCER1_PID37;  // Enable the ADC peripheral
+
+  ADC->ADC_CR = ADC_CR_SWRST;                           // Reset ADC
+  ADC->ADC_PTCR = (ADC_PTCR_RXTDIS | ADC_PTCR_TXTDIS);  // Reset PDC transfer
+
+  ADC->ADC_MR |= ADC_MR_TRGEN_EN             // Hardware trigger select
+                 | ADC_MR_TRGSEL_ADC_TRIG1;  // Trigger by TIOA0
+  ADC->ADC_MR = ADC_MR_PRESCAL(255)          // Set the prescaler to 255
+                | ADC_MR_STARTUP_SUT64       // Set the startup time to 64 periods of ADC_CLK
+                | ADC_MR_TRACKTIM(15)        // Set the tracking time to 15 periods of ADC_CLK
+                | ADC_MR_SETTLING_AST3;      // Set the settling time to 17 periods of ADC_CLK
+  ADC->ADC_CHER = ADC_CHER_CH7;              // Enable channel 7 (A0)
+
+  ADC->ADC_IDR = ~(1u << 7);  // Désactiver toutes les interruptions sauf celle de CH7
+  ADC->ADC_IER = (1u << 7);   // Activer l'interruption pour CH7
+
+  // DMA configuration
+  bufferFull = false;
+
+  PMC->PMC_PCER1 |= PMC_PCER1_PID39;  // Active le périphérique PDC
+  ADC->ADC_PTCR = ADC_PTCR_RXTDIS | ADC_PTCR_TXTDIS;
+  ADC->ADC_RPR = (unsigned long)adcBuffer;  // Adresse de la mémoire de réception
+  ADC->ADC_RCR = (unsigned int)bufferSize;  // Nombre d'éléments à recevoir
+  ADC->ADC_RNPR = 0;                        // Adresse du prochain buffer à charger
+  ADC->ADC_RNCR = 0;                        // Nombre d'éléments du prochain buffer à charger
+
+  NVIC_EnableIRQ(ADC_IRQn);        // Activer l'interruption ADC dans le NVIC
+  ADC->ADC_PTCR = ADC_PTCR_RXTEN;  // Activation du transfert de réception du PDC
 }
 
+// Timer TC0 channel 0 configuration
+void setupTimer() {
+  // Configure Timer Counter 0 Channel 0 (TC0) for Fe1
+  PMC->PMC_PCER0 |= PMC_PCER0_PID27;                                       // Enable the TC0 peripheral
+  TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK4 | TC_CMR_CPCTRG;  // Set the clock source to TCLK4 (MCK / 128, 84 MHz / 128 = 656.25 kHz)
+  // Enable the RC compare trigger
+  // Set the RC value for a Fe1 Hz frequency
+  TC0->TC_CHANNEL[0].TC_RC = 656250 / samplingFrequency - 1;
+  TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;  // Enable the timer counter and trigger it
+  TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;                  // Enable the RC compare interrupt
+  NVIC_EnableIRQ(TC0_IRQn);                                 // Enable the TC0_IRQn interrupt in the NVIC
+}
+
+// Interruption handler for the TCO timer
 void TC0_Handler() {
   // Read the status register to clear the interrupt flag
   TC0->TC_CHANNEL[0].TC_SR;
@@ -97,62 +128,34 @@ void TC0_Handler() {
   ADC->ADC_CR = ADC_CR_START;
 }
 
-int16_t applyFIRFilter(int16_t inputValue) {
-  filterBuffer[filterIndex] = inputValue;
-  int32_t outputValue = 0;
-  int filterTapIndex = filterIndex;
-
-  for (int i = 0; i < FILTER_TAP_NUM; i++) {
-    outputValue += filter_taps[i] * filterBuffer[filterTapIndex];
-
-    filterTapIndex--;
-    if (filterTapIndex < 0) {
-      filterTapIndex = FILTER_TAP_NUM - 1;
-    }
+// Interruption handler for the ADC
+void ADC_Handler() {
+  ADC->ADC_CDR[7];
+  unsigned long status = ADC->ADC_ISR;
+  if (status & ADC_ISR_ENDRX) {
+    bufferFull = true;
   }
-
-  filterIndex++;
-  if (filterIndex >= FILTER_TAP_NUM) {
-    filterIndex = 0;
-  }
-
-  return outputValue >> 15;  // Adjust the shift value according to the number of filter taps used
-}
-
-void record() {
-  // Read the ADC value from the A0 pin
-  uint32_t adcValue = ADC->ADC_CDR[7];
-  // uint16_t filtervalue = applyFIRFilter((uint16_t)adcValue);
-  // uint8_t value = (uint8_t)(filtervalue >> 4);
-  // Print the ADC value to the serial monitor
-  //Serial.write(adcValue);
-  analogWrite(DAC1, adcValue);
-  uint32_t dacOutput = analogRead(dacReaderPin);
-  Serial.println(dacOutput);
-  // Delay for a short period of time to control the sampling rate
-  delay(1000 / samplingFrequency);
 }
 
 void setup() {
-  Serial.begin(460800);
+  Serial.begin(115200);
   setupADC();
-  // Enable the timer counter and trigger it
-  TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
-
+  setupTimer();
   // Setup buttons and led
   pinMode(ledPin, OUTPUT);
   pinMode(buttonRecordingPin, INPUT);
   pinMode(buttonFrequencyPin, INPUT);
   pinMode(dacReaderPin, INPUT);
 
-  previousTime = millis();
+  digitalWrite(ledPin, HIGH);  // Led on
+                               //previousTime = millis();
 }
 
 void loop() {
-  currentTime = millis();
+  //currentTime = millis();
   // Lire l'état du bouton
 
-  if (digitalRead(buttonRecordingPin) == HIGH && !isBtnRecordingPressed) {
+  /*if (digitalRead(buttonRecordingPin) == HIGH && !isBtnRecordingPressed) {
     isBtnRecordingPressed = true;
   } else {
     isBtnRecordingPressed = false;
@@ -166,12 +169,11 @@ void loop() {
   if (isRecording) {
     if (currentTime - previousTime < 10000) {
       digitalWrite(ledPin, HIGH);  // Led on
-      record();
     } else {
       digitalWrite(ledPin, LOW);  // Led off
       isRecording = false;
     }
-  }
+  }*/
 
   //Frequency choice:
   int inputFrequencyChoice = digitalRead(buttonFrequencyPin);
@@ -186,27 +188,58 @@ void loop() {
     isBtnFrequencyPressed = false;
   }
 
-  switch (frequencyState){
-    case f8k:{
-      samplingFrequency = 8000;
-      break;
-    }
-    case f16k:{
-      samplingFrequency = 16000;
-      break;
-    }
-    case f32k:{
-      samplingFrequency = 32000;
-      break;
-    }
-    case f44k:{
-      samplingFrequency = 44100;
-      break;
-    }
-    defalut: {
-      samplingFrequency = 44100;
-      break;
+  if (isBtnFrequencyPressed) {  // When we change sampling frequency
+    switch (frequencyState) {
+      case f8k:
+        {
+          samplingFrequency = 8000;
+          setupTimer();
+          break;
+        }
+      case f16k:
+        {
+          samplingFrequency = 16000;
+          setupTimer();
+          break;
+        }
+      case f32k:
+        {
+          samplingFrequency = 32000;
+          setupTimer();
+          break;
+        }
+      case f44k:
+        {
+          samplingFrequency = 44100;
+          setupTimer();
+          break;
+        }
+      default:
+        {
+          samplingFrequency = 44100;
+          setupTimer();
+          break;
+        }
     }
   }
 
+  //DAC :
+  uint32_t adcValue = ADC->ADC_CDR[7]; //We read the value from the ADC
+  analogWrite(DAC1, adcValue); // We write the value readed with the ADC on the DAC1 PIN
+  uint32_t dacValue = analogRead(dacReaderPin); //We read the output of the DAC after one resistor(perturbations problems);
+  Serial.println(dacValue); //We write the value in the serial plotter
+
+
+  /* if (bufferFull) {
+    for (int i = 0; i < bufferSize; i++) {
+      Serial.print("i: ");
+     Serial.println(i);
+     Serial.println(adcBuffer[i]);
+    }
+    Serial.println("----------");
+    ADC->ADC_RNPR = (unsigned long)adcBuffer;  // Adresse du prochain buffer à charger
+    ADC->ADC_RNCR = (unsigned int)bufferSize;  // Nombre d'éléments du prochain buffer à charger
+    bufferFull = false;
+  }*/
+  //Serial.println(samplingFrequency);
 }
